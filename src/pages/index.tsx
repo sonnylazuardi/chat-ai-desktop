9091f9ec5cdab6afbfcd1f3ad38ac5c829188d6f
Burak Sakalli <buraksakalli06@gmail.com>
2022-12-05 12:33:41 +0400

    update: redirects to use next.config instead of useEffect

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,5 @@
-import React from "react";
-
 function App() {
-  React.useEffect(() => {
-    window.location.href = "https://chat.openai.com/chat";
-  }, [])
-  return (
-    <div className="container"></div>
-  );
+  return <div className="container"></div>;
 }
 
 export default App;
